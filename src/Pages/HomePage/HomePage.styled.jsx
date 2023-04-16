00139fa497d67b427654b8dbd4dc2d26811eeb8d
Demianovq <110334010+Demianovq@users.noreply.github.com>
2023-04-16 21:19:55 +0300

    adds

--- src/Pages/HomePage/HomePage.styled.jsx
+++ src/Pages/HomePage/HomePage.styled.jsx
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
+import Image from 'images/mesh-1430107.png';
 
 export const HeroTitle = styled('h1')`
   font-weight: 900;
   font-size: 44px;
   line-height: calc(60 / 44);
   text-align: center;
-  color: #3d3d3d;
+  color: #fff;
   display: block;
   width: 696px;
   font-family: 'Morganite';
@@ -14,7 +15,7 @@ export const HeroTitle = styled('h1')`
   letter-spacing: 0.06em;
   text-transform: uppercase;
   display: block;
-  text-shadow: 2px 2px 3px pink;
+  text-shadow: 2px 2px 3px black;
 
   margin-left: auto;
   margin-right: auto;
@@ -23,6 +24,14 @@ export const HeroTitle = styled('h1')`
 export const HeroSection = styled('section')`
   padding-top: 300px;
   padding-bottom: 300px;
+  background-image: linear-gradient(
+      rgba(47, 48, 58, 0.4),
+      rgba(47, 48, 58, 0.4)
+    ),
+    url(${Image});
+  background-repeat: no-repeat;
+  background-size: cover;
+
   /* background-image: linear-gradient(to left, #553c9a, #b393d3); */
   background-color: #ffffff;
 `;
@@ -33,23 +42,60 @@ export const HeroBtn = styled('button')`
 
 export const StyledHeroLink = styled(NavLink)`
   display: block;
+  line-height: 42px;
+  font-family: 'Rubik', sans-serif;
+  font-size: 19px;
+  font-weight: 500;
+  margin-top: 30px;
   text-decoration: none;
-  color: #3d3d3d;
-  font-weight: 700;
-  font-size: 16px;
-  line-height: calc(30 / 16);
   text-align: center;
-
   align-items: center;
   letter-spacing: 0.06em;
-  margin-top: 50px;
-  background-image: linear-gradient(to left, #553c9a, #b393d3);
+  border: none;
   border-radius: 4px;
-
   width: 150px;
-  padding: 10px 32px;
-
+  padding: 8px 20px;
   margin-left: auto;
   margin-right: auto;
+
   cursor: pointer;
+
+  /* background-image: linear-gradient(to left, #553c9a, #b393d3); */
+  background-color: black;
+  color: #fff;
+
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+
+  outline: none;
+  &:hover {
+    background: transparent;
+    box-shadow: none;
+  }
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 2px;
+    width: 0;
+    height: 2px;
+    background: #fff;
+    box-shadow: -1px -1px 5px 0px black, 7px 7px 20px 0px #0003,
+      4px 4px 5px 0px #0002;
+    transition: 400ms ease all;
+  }
+  &:after {
+    right: inherit;
+    top: inherit;
+    left: 0;
+    bottom: 0;
+  }
+  &:hover:before,
+  &:hover:after {
+    width: 100%;
+    transition: 800ms ease all;
+  }
 `;
