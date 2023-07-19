b45b899af758b83b8ea1b10ef79c9d789c4f5665
Demianovq <110334010+Demianovq@users.noreply.github.com>
2023-07-19 18:30:26 +0300

    adds

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="">
+    <BrowserRouter basename="Click2Dep">
       <App />
     </BrowserRouter>
   </React.StrictMode>
